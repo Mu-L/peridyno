c01aaf875a976be7a60b9669659eeb162d3b5428
clouddon <clouddon@sina.com>
2026-07-05 16:04:26 +0800

    Allow users to configure custom cameras

--- src/Rendering/GUI/ImWidgets/ImCameraAgent.cpp
+++ src/Rendering/GUI/ImWidgets/ImCameraAgent.cpp
@@ -14,10 +14,55 @@ namespace dyno
 		mCamera->setHeight(128);
 		mCamera->registerPoint(0, 0);
 		mCamera->rotateToPoint(-32, 12);
+
+		//TODO: does not work yet, need to check the validation of the callback function
+		this->varViewportSize()->attach(std::make_shared<FCallBackFunc>(
+			[=]() {
+				auto vSize = this->varViewportSize()->getValue();
+				mCamera->setWidth(vSize.x);
+				mCamera->setHeight(vSize.y);
+			}));
+
+		this->varViewportSize()->setValue(Vec2i(256, 256));
 	}
 
 	ImCameraAgent::~ImCameraAgent()
 	{
 
+	}
+
+	void ImCameraAgent::resetStates()
+	{
+		updateCamera();
+	}
+
+	void ImCameraAgent::updateStates()
+	{
+		updateCamera();
+	}
+
+	void ImCameraAgent::updateCamera()
+	{
+		auto parent = this->importParent()->getDerivedNode();
+		if (parent != nullptr)
+		{
+			Vec3f loc = this->varLocalTranslation()->getValue();
+			Vec3f rot = this->varLocalRotation()->getValue();
+
+			Quat1f quat = Quat1f::fromEulerAngles(rot.x / 180.0f * M_PI, rot.y / 180.0f * M_PI, rot.z / 180.0f * M_PI);
+
+			Vec3f parentLoc = parent->stateLocation()->getValue();
+			Quat1f parentRot = parent->stateRotation()->getValue();
+
+			Vec3f worldLoc = parentLoc + parentRot.rotate(loc);
+			Quat1f worldRot = parentRot * quat;
+			worldRot.normalize();
+
+			this->stateLocation()->setValue(worldLoc);
+			this->stateRotation()->setValue(worldRot);
+
+			mCamera->setEyePos(worldLoc);
+			mCamera->setTargetPos(worldLoc + worldRot.rotate(Vec3f(0, 0, 1)));
+		}
 	}
 }
--- src/Rendering/GUI/ImWidgets/ImCameraAgent.h
+++ src/Rendering/GUI/ImWidgets/ImCameraAgent.h
@@ -20,9 +20,19 @@ namespace dyno
 	public:
 		DEF_VAR(bool, Enabled, true, "Enable the camera agent");
 
-		DEF_VAR(Vec2i, ViewportLocation, Vec2i(0), "Bottom left corner of the viewport");
+		DEF_VAR(Vec2i, ViewportLocation, Vec2i(0, 512), "Bottom left corner of the viewport");
+		DEF_VAR(Vec2i, ViewportSize, Vec2i(256, 256), "Viewport size");
+
+		DEF_VAR(Vec3f, LocalTranslation, Vec3f(0), "");
+		DEF_VAR(Vec3f, LocalRotation, Vec3f(0), "");
+
+	protected:
+		void resetStates() override;
+		void updateStates() override;
 
 	private:
+		void updateCamera();
+
 		std::shared_ptr<Camera>		mCamera;
 	};
 }
